5e9c82d5f8f0de0a252cbdd00edf3ecdeba5d80c
Harsh Agrawal <ha0744106@gmail.com>
2026-02-16 22:11:26 +0530

    Update batch_size in get_config function

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 def get_config():
     return {
-        "batch_size" : 10,
+        "batch_size" : 20,
         "num_epochs" : 20,
         "lr" : 10**-4,
         "seq_len" : 350,
@@ -20,4 +20,5 @@ def get_weights_file_path( config, epoch:str):
     model_folder = config['model_folder']
     model_basename = config['model_basename']
     model_filename = f"{model_basename}{epoch}.pt"
+
     return str(Path('.') / model_folder / model_filename)
